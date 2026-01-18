0af3ff109923943d6687875462e0cfd2735f35c9
Bogdan Kolesnik <kolesnik.bg@gmail.com>
2026-01-18 22:46:29 +0300

    запись в файл ссылок картинок #RMNT-KM-2[в работе]

--- parser.php
+++ parser.php
@@ -1,4 +1,10 @@
 <?php
+// подключение библы для excel
+require_once "./PhpSpreadsheet-1.12.0/vendor/autoload.php";
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use \PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\IOFactory; // обработка i-o 
+
 // кодировка
 header('Content-Type: text/html; charset=utf-8');
 
@@ -11,7 +17,7 @@ function fetch_html($url) {
     curl_setopt($ch, CURLOPT_TIMEOUT, 10); // ждем 
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5); // подключаемся 
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // скипаем ssl 
-    curl_setopt($ch, CURLOPT_ENCODING, ''); // Поддержка gzip
+    curl_setopt($ch, CURLOPT_ENCODING, ''); // gzip БЕЗ ЭТОГО НЕ РАБОТАЕТ !!!
 
     // маскировка под браузер
     curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36');
@@ -46,20 +52,57 @@ function extract_image_links($html) {
 // откуда 
 $url = 'https://www.juraprofi.de/DeLonghi-Ersatzteile/Wassertank/Abdeckung-Wassertank-silber-fuer-DeLonghi-Perfecta-Evo::17096.html'; 
 
-echo "<h2>Получение html</h2>";
+// echo "<h2>Получение html</h2>";
 
 // получение html 
 $html = fetch_html($url);
 
-echo "<h2>Извлеение ссылок</h2>";
+// echo "<h2>Извлеение ссылок</h2>";
 $image_links = extract_image_links($html);
 
-if (empty($image_links)) {
-    echo "Не получилось собрать данные";
+// if (empty($image_links)) {
+//     echo "Не получилось собрать данные";
+// } else {
+//     echo "<p>Найдено картинок: " . count($image_links) . "</p>";
+//     echo "<pre>";
+//     print_r($image_links); // массив
+//     echo "</pre>";
+// }
+
+// соединение в массиве "|"
+$links_string = implode('|', $image_links);
+
+// файл
+$filename = 'test.xlsx';
+
+// файл сущ.
+if(file_exists($filename)) {
+    $spreadsheet = IOFactory::load($filename); // открытие файла
+    $sheet = $spreadsheet->getActiveSheet(); // к активной странице
+    $nextRow = $sheet->getHighestRow() + 1; // сдвигаемся к после заполненой
+    $sheet->setCellValue('A' . $nextRow, $links_string); // запись ниже заполненных данных НАДО ДОБАВИТЬ ЛОГИКУ !!!
+    echo "$links_string";
 } else {
-    echo "<p>Найдено товаров: " . count($image_links) . "</p>";
-    echo "<pre>";
-    print_r($image_links); // массив
-    echo "</pre>";
+    $spreadsheet = new Spreadsheet(); // новая табла
+    $sheet = $spreadsheet->getActiveSheet();// к активной странице таблы
+    // заполнение таблицы
+    $sheet->setCellValue('A1', 'Картинки');
+    $sheet->setCellValue('A2', $links_string);
+    echo "Создан файл";
 }
+
+
+// // подготовка к скачиванию
+// header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+// header('Content-Disposition: attachment; filename="' . $filename . '"');
+
+// // скачивем файл 
+// $writer = new Xlsx($spreadsheet);
+// $writer->save('php://output');
+
+// сохранение файла на сервере 0_о
+$writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+$writer->save($filename);
+echo "Файл сохранен " . realpath($filename);
+// exit;
 ?>
